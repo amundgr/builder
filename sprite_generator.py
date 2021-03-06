e04460a380009ce7172076d073a57b85157199ba
Amund Grorud <amund@grorud.net>
2021-03-07 00:35:45 +0100

    fix for even number diameter light

--- sprite_generator.py
+++ sprite_generator.py
@@ -2,13 +2,13 @@ import numpy as np
 import cv2
 
 def generate_light(radius, inner_radius, number_of_steps=10):
-    board = np.zeros((radius*2, radius*2))
-    center = radius-1
+    diameter = int(2 * radius)
+    board = np.zeros((diameter, diameter))
     radius_diff = radius - inner_radius
-    for x in range(radius*2-1):
-        for y in range(radius*2-1):
-            distance = np.sqrt((center - x)**2 + (center - y)**2)
-            if distance > inner_radius:
+    for x in range(diameter):
+        for y in range(diameter):
+            distance = np.sqrt((radius - x - 0.5)**2 + (radius - y - 0.5)**2)
+            if distance >= inner_radius:
                 board[x,y] = (distance - inner_radius + 1) / radius_diff
             else:
                 board[x,y] = 0
@@ -22,4 +22,13 @@ def generate_light(radius, inner_radius, number_of_steps=10):
     return np.round(board, 1)
 
 if __name__ == "__main__":
-    generate_light(32, 10)
+    radius = 11
+    inner_radius = 2
+    number_of_steps = 6
+    res = generate_light(radius=radius, 
+                         inner_radius=inner_radius, 
+                         number_of_steps=number_of_steps)
+
+    cut_out_radius = 5
+    print(res[radius-cut_out_radius:radius+cut_out_radius, 
+              radius-cut_out_radius:radius+cut_out_radius])
